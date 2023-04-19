c13ba36a2ef4445cbbf517c7f9297e82022e49ab
Wolodia Zdetovetzky <wolozdeto@gmail.com>
2023-04-19 13:40:41 +0200

    init arbre binaire, genetic

---
+++ Genetic/GeneticTSP.py
@@ -0,0 +1,13 @@
+import os
+
+class PVC_Genetique:
+
+    def clear_term(self):
+        os.system('cls' if os.name=='nt' else 'clear')
+    
+def main():
+    # A compléter
+    return
+
+if __name__ == "__main__":
+    main()
